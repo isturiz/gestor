f11d8d38e7d4fd113279df9f5fa5c0e7b499239f
isturiz <isturizmauricio@gmail.com>
2022-11-01 03:43:38 -0400

    first commit

---
+++ Vista/includes/headers/header.php
@@ -0,0 +1,25 @@
+<header>
+  <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <div class="flex flex-initial justify-between items-center mx-auto max-w-screen-xl">
+      <a href="https://flowbite.com" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SGT</span>
+      </a>
+
+      <!-- buscador -->
+      <div class="flex justify-center items-center">
+        <div class="xl:w-96">
+          <div class="input-group relative flex items-stretch w-full mb-4">
+            <input type="search" class="flex relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Buscar archivo" aria-label="Search">
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center lg:order-2">
+        <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Usuario</a>
+
+      </div>
+
+    </div>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.tailwindcss.com"></script>
+
+<?php
+require_once 'Vista/includes/headers/header.php';
+require_once 'Vista/includes/headers/aside.php';
+?>
