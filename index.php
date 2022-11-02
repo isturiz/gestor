b0a13ff265de0c032ada444faa7e61bca7572783
isturiz <isturizmauricio@gmail.com>
2022-11-02 19:47:34 -0400

    add templates

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
 <?php
-// require_once 'Vista/includes/headers/header.php';
-// require_once 'Vista/includes/headers/aside.php';
 require_once 'Vista/includes/headers/headerMain.php';
-require_once 'Vista/sistema/login.php';
+require_once 'Vista/includes/headers/header.php';
+require_once 'Vista/includes/headers/aside.php';
+require_once 'Vista/gestor/listaArchivo.php';
+require_once 'Vista/gestor/subirArchivo.php';
+// require_once 'Vista/sistema/login.php';
 
 ?>
