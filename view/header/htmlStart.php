b5a70272c1394523bfad2ef0dc24fada167da8e1
isturiz <isturizmauricio@gmail.com>
2022-11-06 18:28:37 -0400

    add icon page

--- view/header/htmlStart.php
+++ view/header/htmlStart.php
@@ -5,7 +5,9 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Gestor</title>
+  <title>SGT | Gestor</title>
+
+  <link rel="shortcut icon" href="https://flowbite.com/docs/images/logo.svg">
 
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
