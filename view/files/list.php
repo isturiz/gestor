2a093eaed1f726d6dedca0f787029f36819b864f
isturiz <isturizmauricio@gmail.com>
2022-12-06 22:44:11 -0400

    add file browser

--- view/files/list.php
+++ view/files/list.php
@@ -60,6 +60,8 @@
               </ul>
             </div>
           </div>
+
+          <!-- Search -->
           <label for="table-search" class="sr-only">Search</label>
           <div class="relative">
             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -67,11 +69,12 @@
                 <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
               </svg>
             </div>
-            <input type="text" id="table-search" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
+            <input type="text" id="table-search" onkeyup="doSearch()" class="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar">
           </div>
+
         </div>
 
-        <table class="w-full text-sm text-left text-gray-400">
+        <table class="w-full text-sm text-left text-gray-400" id="table-file">
           <thead class="text-xs uppercase bg-gray-700 text-gray-400">
             <tr>
               <th scope="col" class="py-3 px-6">Nombre</th>
@@ -123,10 +126,16 @@
 
             }
             ?>
+
+            <tr class='noSearch hide'>
+              <td colspan="5"></td>
+            </tr>
           </tbody>
         </table>
       </div>
     </main>
   </div>
+
+  <script src="../../search.js"></script>
 
   <?php require_once('../../view/header/htmlEnd.php'); ?>
